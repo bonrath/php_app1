1d936e025f8cbbfce6b018933429ade67953ce90
bonrathroeuth <bonrathroeuth@gmail.com>
2024-06-02 16:31:10 +0700

    vd

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="index.php" class="navbar-brand">App3</a>
+            <a href="index.php" class="navbar-brand">App4</a>
         </div>
 
         <div class="collapse navbar-collapse" style="display: none!important;" id="navbar-collapse">
